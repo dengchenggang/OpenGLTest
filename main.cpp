57d9087c7d99d10d6f42d14fb6b094474f06f113
路过的小黑 <1175029896@qq.com>
2022-11-23 23:07:44 +0800

    增加glm

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include "scripts/Test.h"
+#include "scripts/OpenGLInteraface/COpenGLContext.h"
 #include <iostream>
 
 
 int main(int argc, char *argv[]) {
-    Test();
+    auto opengl = std::make_shared<COpenGLRectangle>("Test");
+    opengl->SetVertextShader("./scripts/shader/testVertexShader.glsl");
+    opengl->SetFragmentShader("./scripts/shader/testFragmentShader.glsl");
+    opengl->SetTexture1("./scripts/data/resources/textures/container.jpg");
+    opengl->SetTexture2("./scripts/data/resources/textures/awesomeface.png");
+    if (opengl->Init()) {
+        opengl->Update();
+    }
     return 0;
 }
